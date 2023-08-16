66c2069c9a5080df18a8893a9dce94c652bbee34
Zakaria Kadoussi <zakaria.kado@gmail.com>
2023-08-16 10:59:17 +0200

    vf post soutenance format email invalid

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,44 +1,50 @@
-const User = require('../models/user');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const User = require("../models/user");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 exports.signup = (req, res, next) => {
-    bcrypt.hash(req.body.password, 10)
-    .then(hash => {
-        const user = new User ({
-            email: req.body.email,
-            password: hash
-        });
-        user.save()
+  // Vérification du format de l'email
+  if (!/^\S+@\S+\.\S+$/.test(req.body.email)) {
+    return res.status(400).json({ message: "Email format is invalid" });
+  }
+
+  bcrypt
+    .hash(req.body.password, 10)
+    .then((hash) => {
+      const user = new User({
+        email: req.body.email,
+        password: hash,
+      });
+      user
+        .save()
         .then(() => res.status(201).json({ message: "User created" }))
-        .catch(error => res.status(400).json({ error }));
+        .catch((error) => res.status(400).json({ error }));
     })
-    .catch(error => res.status(500).json({ error }));
+    .catch((error) => res.status(500).json({ error }));
 };
 
 exports.login = (req, res, next) => {
-    User.findOne({ email: req.body.email })
-    .then(user => {
-        if(!user){
-            res.status(400).json({ message: "Identifiants incorrects" });
-        } else {
-            bcrypt.compare(req.body.password, user.password)
-            .then(valid => {
-                if(!valid){
-                    res.status(400).json({ message: "Identifiants incorrects" });
-                } else {
-                    res.status(200).json({
-                        userId: user._id,
-                        token: jwt.sign(
-                            { userId: user._id },
-                            'T1mes00KYt9C',
-                            { expiresIn: '24h' }
-                        )
-                    });
-                }
-            })
-            .catch(error => res.status(400).json({ error }));
-        }
+  User.findOne({ email: req.body.email })
+    .then((user) => {
+      if (!user) {
+        res.status(400).json({ message: "Identifiants incorrects" });
+      } else {
+        bcrypt
+          .compare(req.body.password, user.password)
+          .then((valid) => {
+            if (!valid) {
+              res.status(400).json({ message: "Identifiants incorrects" });
+            } else {
+              res.status(200).json({
+                userId: user._id,
+                token: jwt.sign({ userId: user._id }, "T1mes00KYt9C", {
+                  expiresIn: "24h",
+                }),
+              });
+            }
+          })
+          .catch((error) => res.status(400).json({ error }));
+      }
     })
-    .catch(error => res.status(400).json({ error }));
+    .catch((error) => res.status(400).json({ error }));
 };
